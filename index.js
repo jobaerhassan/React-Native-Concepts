9979e06c111723004917d6e5b6816adf2b2cc5cb
jobaer-hassan <jobaerhassan3@gmail.com>
2023-12-29 00:17:25 +0600

    feat: setup

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import MainIndex from './src/MainIndex';
+const App = () => {
+    return <MainIndex/>
+}
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/MainIndex.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+const MainIndex = () => {
+    return(
+        <View>
+            <Text>Hello world</Text>
+        </View>
+    )
+}
+export default MainIndex;
