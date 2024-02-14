0faee4ad4e5549c4ae4d1185fc32bfbfa335b687
Jobaer-hassan-orangetoolz <jobaer@orangetoolz.com>
2024-02-14 07:56:39 +0600

    upload csv work ongoing

--- src/features/UploadCSV.jsx
+++ src/features/UploadCSV.jsx
@@ -1,15 +1,30 @@
 /* eslint-disable react/react-in-jsx-scope */
+import {useState} from 'react';
 import {Button, StyleSheet, View} from 'react-native';
 import DocumentPicker from 'react-native-document-picker';
+import RNFS from 'react-native-fs';
 const UploadCSV = () => {
+  const [selectedDoc, setSelectedDoc] = useState(null);
   const selectFiles = async () => {
     try {
       const doc = await DocumentPicker.pick({
-        type: DocumentPicker.types.pdf,
+        type: DocumentPicker.types.allFiles,
       });
-      console.log(doc);
+      const filePath = doc.uri;
+      setSelectedDoc(filePath);
+      readFile();
     } catch (error) {
-      console.log(error);
+      console.log('Error picking document:', error);
+    }
+  };
+  const readFile = async () => {
+    if (selectedDoc) {
+      try {
+        const content = await RNFS.readFile(selectedDoc, 'utf-8');
+        console.log('document content', content);
+      } catch (error) {
+        console.log('error while read file', error);
+      }
     }
   };
   return (
